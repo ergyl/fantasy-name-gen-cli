018f49aafbf8777e909260eef235b6ed5ba1184d
Erik Gylleus <144366622+ergyl@users.noreply.github.com>
2026-07-15 01:56:48 +0200

    add feature availability configuration (WIP)

---
+++ FantasyNameGenerator/Infrastructure/Configuration/RaceConfig.cs
@@ -0,0 +1,35 @@
+public static class RaceCatakog
+{
+    public record RaceConfig(
+      Race Race,
+      bool Enabled);
+
+
+/// <summary>
+/// A configuration class that holds the availability of different data.
+/// To be used to encapsule the availability of different features in the generator.
+/// </summary>
+    public interface IFeatureAvailabilityConfig
+    {
+        IReadOnlyList<RaceConfig> Races { get; }
+        bool IsRaceSupported(Race race);
+    }
+
+    public class FeatureAvailabilityConfig : IFeatureAvailabilityConfig
+    {
+        public FeatureAvailabilityConfig(params RaceConfig[] races)
+        {
+            Races = races;
+        }
+
+        public IReadOnlyList<RaceConfig> Races { get; }
+
+        /// <summary>
+        /// To check if a race is currently supported by the generator.
+        /// </summary>
+        /// <param name="race">The race to check.</param>
+        /// <returns>False if the race is not supported, true otherwise.</returns>
+        public bool IsRaceSupported(Race race)
+            => Races.Any(r => r.Race == race && r.Enabled);
+    }
+}
