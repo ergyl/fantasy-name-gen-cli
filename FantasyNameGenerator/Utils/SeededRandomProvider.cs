e5ac990798afc10f3ee5a33794fdb763aa1ed55d
Erik Gylleus <144366622+ergyl@users.noreply.github.com>
2026-07-22 22:51:33 +0200

    simplify and SeededRandomProvider

--- FantasyNameGenerator/Utils/SeededRandomProvider.cs
+++ FantasyNameGenerator/Utils/SeededRandomProvider.cs
@@ -1,5 +1,8 @@
 namespace FantasyNameGenerator.Utils;
 
+/// <summary>
+/// Simple pseudo-random number generator (PRNG) that can be seeded for deterministic behavior.
+/// </summary> 
 public class SeededRandomProvider : ISeededRandom
 {
     private readonly Random _rng;
@@ -8,9 +11,10 @@ public class SeededRandomProvider : ISeededRandom
 
     public SeededRandomProvider(long seed)
     {
-        /* Non-deterministic behavior.
-        Pseudo-random number generator (PRNG) seeded with a long value.*/
-        _rng = new Random((int)(seed & 0xFFFFFFFF));
+        /* Initialize the random number generator with the provided seed.
+        This means that the sequence of random numbers generated will be the same for the same seed, which is useful for testing and reproducibility.
+        For deterministic behavior, the seed can be set to a specific value. If no seed is provided, the current environment tick count is used, which will vary each time the application runs. */
+        _rng = new Random((int)seed);
     }
 
     public int Next(int maxValue) => _rng.Next(maxValue);
